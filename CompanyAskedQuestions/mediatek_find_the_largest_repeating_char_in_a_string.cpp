000b29e6d2a5b9371eb785add49ceba184cba726
ANIRBAN SENGUPTA <aniduino@gmail.com>
2022-03-07 11:52:01 +0530

    Create mediatek_find_the_largest_repeating_char_in_a_string.cpp

---
+++ CompanyAskedQuestions/mediatek_find_the_largest_repeating_char_in_a_string.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+// find the index of the largest repeating char
+
+using namespace std;
+
+char const *pString = "aabbbcccc";
+map<int, int, greater<int>> myMap;
+
+int main() {
+
+    int max = 0;
+    uint8_t len = strlen((char const *) pString);
+
+    for (int itr = 0; itr <= len; itr++) {
+        if (*(pString + itr) == *(pString + itr + 1)) {
+            // increment max
+            max++;
+        } else {
+            //reset max and store
+            myMap.insert(make_pair(max + 1, itr - (max)));
+            max = 0;
+        }
+    }
+
+
+    auto it = myMap.begin();
+    printf("result : %d", (*it).second);
+    return 0;
+}
