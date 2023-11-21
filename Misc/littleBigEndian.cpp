d3023e3d5df1f8f29394c8b461393e8959409046
Anirban Sengupta <aniduino@gmail.com>
2023-11-21 17:36:06 +0100

    Fix minor example

--- Misc/littleBigEndian.cpp
+++ Misc/littleBigEndian.cpp
@@ -4,21 +4,19 @@
 using namespace std;
 int main()
 {
-    uint32_t value = 0x11223344;
+    uint32_t value = 0x44332211;
     //                MSB    LSB
 
     uint8_t *pTtest = (uint8_t *)&value;
 
     printf("Data at lowest address: 0x%x\n", *pTtest);
 
-    char *c = (char *)&value;
-    if (*c == 0x44)
+    if (*pTtest == 0x11)
     {
         printf("Little endian: As lowest address has MSB\n");
         printf("Data: 0x44 0x33 0x33 0x22 0x11\n");
         printf("Add : 0x00 0x01 0x02 0x03 0x04\n");
     }
-
     else
         printf("Big endian");
 }
