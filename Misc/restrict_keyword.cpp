ca3bbc9a0d40b5f506bd47d270316bb2a6e76298
Anirban Sengupta <aniduino@gmail.com>
2023-11-23 19:40:21 +0100

    Restrict key word example

---
+++ Misc/restrict_keyword.cpp
@@ -0,0 +1,3 @@
+// practice from ARM and GeeksforGeeks
+// https://learn.arm.com/learning-paths/cross-platform/restrict-keyword-c99/what-is-restrict/
+// https://www.geeksforgeeks.org/restrict-keyword-c/
