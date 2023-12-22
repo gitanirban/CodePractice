189e5681f318486d0702a34fd05cceb4d132169e
Anirban Sengupta <aniduino@gmail.com>
2023-12-22 22:30:11 +0100

    Add CRTP Design Pattern

---
+++ C++_Practice_on_STL/DesignPattern/CRTP.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+template<class DERIVED>
+class Base {
+public:
+    void speak() { (reinterpret_cast<DERIVED*>(this))->vocalise(); }
+};
+
+class Cat : public Base<Cat> {
+public:
+    void vocalise() { cout << "Meow " << endl; }
+};
+
+class Dog : public Base<Dog> {
+public:
+    void vocalise() { cout << "Vow  " << endl; }
+};
+
+int main() {
+
+    Base<Cat> cat;
+    Base<Dog> dog;
+
+    cat.speak();
+    dog.speak();
+
+    Cat cat2;
+    cat2.speak();
+
+    return 0;
+}
