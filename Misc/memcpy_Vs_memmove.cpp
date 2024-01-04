bf282085d9afba037f17bdbf33dfc7da730d1b34
Anirban Sengupta <aniduino@gmail.com>
2024-01-04 12:28:44 +0100

    Add memcpy and memmove

---
+++ Misc/memcpy_Vs_memmove.cpp
@@ -0,0 +1,20 @@
+#include <stddef.h> /* for size_t */
+#include <stdlib.h> /* for memcpy */
+
+#include <stddef.h> /* size_t */
+void* memcpy(void* dest, const void* src, size_t n)
+{
+    char* dp = dest;
+    const char* sp = src;
+    while (n--)
+        *dp++ = *sp++;
+    return dest;
+}
+
+void* memmove(void* dest, const void* src, size_t n)
+{
+    unsigned char tmp[n];
+    memcpy(tmp, src, n);
+    memcpy(dest, tmp, n);
+    return dest;
+}
