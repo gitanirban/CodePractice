cc68a610375c9314642674033fe63abd76e9d9d6
Anirban Sengupta <aniduino@gmail.com>
2024-05-01 19:07:34 +0200

    -minor

--- CompanyAskedQuestions/QC_Count_Distinct_pair.cpp
+++ CompanyAskedQuestions/QC_Count_Distinct_pair.cpp
@@ -2,24 +2,25 @@
 
 #include <iostream>
 #include <unordered_set>
-//#include "stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
 int countDistinctPair();
 void my_init();
-unordered_set<int> my_set;
+
 unordered_set<int> my_visited_Set;
 int const sz = 5;
-int arr[sz] = { 1 ,1 ,2, 3, 5 };
+vector<int> arr = { 1 ,1 ,2, 3, 5 };
 int diff = 1;
 int main()
 {
-	my_init();
+ 
 	cout << countDistinctPair() << endl;
 }
 
 
 int countDistinctPair() {
+	unordered_set<int> my_set(arr.begin(), arr.end());
 	int res = 0;
 	for (int i = 0; i < sz; i++) {
 		if ((my_set.find(arr[i] + diff) != my_set.end()) && (my_visited_Set.find(arr[i]) == my_visited_Set.end()))
@@ -29,9 +30,4 @@ int countDistinctPair() {
 		}
 	}
 	return res;
-}
-void my_init()
-{
-	for (int i = 0; i < sz; i++)
-		my_set.insert(arr[i]);
 }
