313baaafabf7ec9b079562ab231b0ed261be011f
Anirban Sengupta <aniduino@gmail.com>
2024-05-02 11:20:10 +0200

    -minor

--- CompanyAskedQuestions/QC_Snake.cpp
+++ CompanyAskedQuestions/QC_Snake.cpp
@@ -15,20 +15,20 @@ int arr[5][5] =
 int main()
 {
     cout << "Hello world!" << endl;
-    for(int i = 0; i<6; i++)
+    for(int i = 0; i<5; i++)
     {
         if(i%2 == 0)
         {
             for(int j=0; j<5; j++)
             {
-                cout<< arr[j][i] ;
+                cout<< arr[i][j] ;
             }
         }
         else
         {
             for(int j=4; j>=0; j--)
             {
-                cout<< arr[j][i] ;
+                cout<< arr[i][j] ;
             }
         }
 
