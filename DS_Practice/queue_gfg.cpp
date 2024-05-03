11bd2f6bc035e5a400ca00061d0bf3fdc0eb5c98
Anirban Sengupta <aniduino@gmail.com>
2024-05-03 16:57:25 +0200

    - minor

--- DS_Practice/queue_gfg.cpp
+++ DS_Practice/queue_gfg.cpp
@@ -2,14 +2,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Queue{
+class Queue {
 public:
-    uint8_t m_front,m_rear,m_size, m_capacity;
-    int8_t * m_array;
+    uint8_t m_front, m_rear, m_size, m_capacity;
+    int8_t* m_array;
 };
 
-Queue* createQueue(uint8_t capacity){
-    Queue * pQueue = new Queue();
+Queue* createQueue(uint8_t capacity) {
+    Queue* pQueue = new Queue();
     pQueue->m_capacity = capacity;
     pQueue->m_front = pQueue->m_size = 0U;
     pQueue->m_rear = capacity - 1U;
@@ -17,15 +17,15 @@ Queue* createQueue(uint8_t capacity){
     return pQueue;
 }
 
-int isFull(Queue * pQueue){
+int isFull(Queue* pQueue) {
     return (pQueue->m_size == pQueue->m_capacity);
 }
-int isEmpty(Queue * pQueue){
+int isEmpty(Queue* pQueue) {
     return (pQueue->m_size == 0U);
 }
 
-void enqueue(Queue * pQueue, int8_t data){
-    if(isFull(pQueue))
+void enqueue(Queue* pQueue, int8_t data) {
+    if (isFull(pQueue))
         return;
     pQueue->m_rear = (pQueue->m_rear + 1U) % pQueue->m_capacity;
     pQueue->m_array[pQueue->m_rear] = data;
@@ -34,13 +34,13 @@ void enqueue(Queue * pQueue, int8_t data){
 
 }
 
-int dequeue(Queue * pQueue){
-    if(isEmpty(pQueue))
+int dequeue(Queue* pQueue) {
+    if (isEmpty(pQueue))
         return INT_MIN;
-    int8_t data =  pQueue->m_array[pQueue->m_front];
+    int8_t data = pQueue->m_array[pQueue->m_front];
     pQueue->m_front = (pQueue->m_front + 1U) % pQueue->m_capacity;
     pQueue->m_size -= 1U;
-return data;
+    return data;
 
 }
 
@@ -65,28 +65,28 @@ int rear(Queue* queue)
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    Queue *pQ =  createQueue(5);
+    Queue* pQ = createQueue(5);
 
     enqueue(pQ, 10);
     cout << "Front item is "
-         << front(pQ) << endl;
+        << front(pQ) << endl;
     cout << "Rear item is "
-         << rear(pQ) << endl;
+        << rear(pQ) << endl;
     enqueue(pQ, 20);
     cout << "Front item is "
-         << front(pQ) << endl;
+        << front(pQ) << endl;
     cout << "Rear item is "
-         << rear(pQ) << endl;
+        << rear(pQ) << endl;
     enqueue(pQ, 30);
     cout << "Front item is "
-         << front(pQ) << endl;
+        << front(pQ) << endl;
     cout << "Rear item is "
-         << rear(pQ) << endl;
+        << rear(pQ) << endl;
     enqueue(pQ, 40);
     cout << "Front item is "
-         << front(pQ) << endl;
+        << front(pQ) << endl;
     cout << "Rear item is "
-         << rear(pQ) << endl;
+        << rear(pQ) << endl;
 
     return 0;
 }
